def1ab00f0a3b3c8a1e545bc5134d101f375e975
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-15 19:29:49 +0600

    Time: 0 ms (100%), Space: 20 MB (83.9%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int profit = 0;
+
+        for(int i = 1; i < prices.size(); i++)
+        {
+            if(prices[i - 1] < prices[i])
+            {
+               profit += prices[i] - prices[i - 1]; 
+            }
+        }
+        return profit;
+
+    }
+};
