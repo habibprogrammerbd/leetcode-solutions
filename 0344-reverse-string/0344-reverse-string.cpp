6d6e6710a056f0eff0bd3dd6778be30a3695c7a7
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-16 10:45:59 +0600

    Time: 1 ms (9.61%), Space: 27.4 MB (15.54%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         
         int left = 0, right = s.size() -1;
         while(left <= right)
