0abe8cd3b010fb8fb7b1962d20c05171152d3b49
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-17 10:41:16 +0600

    Time: 26 ms (88.08%), Space: 82.8 MB (89.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,15 +4,11 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        multiset<int> st;
-        for(int i = 0; i < nums.size(); i++)
+        sort(nums.begin(),nums.end());
+
+        for(int i = 0; i < nums.size() -1; i++)
         {
-            st.insert(nums[i]);
-        }
-        
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(st.count(nums[i]) >= 2)
+            if(nums[i] == nums[i + 1])
             {
                 return true;
             }
