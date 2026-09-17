21f027ed8ca26531912123bb54ddf327cf64b930
Ahosan Habib <habibprogrammerbd@gmail.com>
2026-09-17 10:04:40 +0600

    Time: 3 ms (44.92%), Space: 9.3 MB (99.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
